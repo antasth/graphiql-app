fd99c3f9e627f3c4813a1a8e4b83cb145105bc4b
antasth <antasth@mail.ru>
2023-12-07 16:45:10 +0300

    feat: add SignIn page

---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,78 @@
+import { Button, Form, Input } from 'antd';
+import { ValidateErrorEntity } from 'rc-field-form/lib/interface';
+import styles from './SignIn.module.scss';
+
+interface ISignInValues {
+  password: string;
+  username: string;
+}
+
+export function SignIn() {
+  const onFinish = (values: ISignInValues) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: ValidateErrorEntity<ISignInValues>) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <section className={styles.signIn}>
+      <Form
+        name="signInForm"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input autoComplete="on" />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password autoComplete="on" />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Sign in
+          </Button>
+        </Form.Item>
+      </Form>
+    </section>
+  );
+}

b51f30eb05490c2dff40dd6de1e8ee63485d8bc3
antasth <antasth@mail.ru>
2023-12-07 14:52:30 +0300

    feat: add position sticky to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
-import { GlobalOutlined, HomeOutlined, LoginOutlined, UserOutlined } from '@ant-design/icons';
+import {
+  CodeOutlined,
+  GlobalOutlined,
+  HomeOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Button, Menu } from 'antd';
+import { Affix, Button, Menu } from 'antd';
 import { Header as AntdHeader } from 'antd/es/layout/layout';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
 
@@ -10,6 +18,11 @@ const menuItems: MenuProps['items'] = [
     label: 'Welcome',
     key: 'welcome',
     icon: <HomeOutlined />,
+  },
+  {
+    label: 'Main',
+    key: 'main',
+    icon: <CodeOutlined />,
   },
   {
     label: 'Sign in',
@@ -21,21 +34,38 @@ const menuItems: MenuProps['items'] = [
     key: 'signup',
     icon: <UserOutlined />,
   },
+  {
+    label: 'Sign out',
+    key: 'signout',
+    icon: <LogoutOutlined />,
+  },
 ];
 
 export const Header = () => {
+  const [isAffixed, setIsAffixed] = useState(false);
   const navigate = useNavigate();
 
   const onClick: MenuProps['onClick'] = (e) => {
     navigate(e.key);
   };
 
+  const handleAffixChange = (affixed?: boolean) => {
+    if (typeof affixed === 'boolean') {
+      setIsAffixed(affixed);
+    }
+  };
+
   return (
-    <AntdHeader className={styles.header}>
-      <Menu onClick={onClick} mode="horizontal" items={menuItems} className={styles.menuStyle} />
-      <Button>
-        <GlobalOutlined /> ru
-      </Button>
-    </AntdHeader>
+    <>
+      <Affix target={() => window} onChange={(affixed?: boolean) => handleAffixChange(affixed)}>
+        <div></div>
+      </Affix>
+      <AntdHeader className={`${styles.header} ${isAffixed ? styles.affixed : ''}`}>
+        <Menu onClick={onClick} mode="horizontal" items={menuItems} className={styles.menu} />
+        <Button className={styles.lang}>
+          <GlobalOutlined /> ru
+        </Button>
+      </AntdHeader>
+    </>
   );
 };

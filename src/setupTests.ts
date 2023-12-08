4692326dc986a4df6d0c05e64206540fc7d6dabf
Iurii Bazhenov <bazhenovyn@gmail.com>
2023-12-08 12:09:57 +0100

    feat: implement context tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,17 +1,22 @@
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import * as context from './context/TranslateContext';
 import { AVAILABLE_LANGUAGES, DEFAULT_LANGUAGE } from './constants/languages';
 
 afterEach(() => {
   cleanup();
 });
 
-vi.mock('@/context/TranslateContext', () => ({
-  useTranslate: () => ({
-    language: DEFAULT_LANGUAGE,
-    availableLanguages: AVAILABLE_LANGUAGES,
-    t: (key: string, value?: string) => value ?? key,
-    setLanguage: vi.fn(),
-  }),
-}));
+vi.mock('@/context/TranslateContext', async () => {
+  const actual: typeof context = await vi.importActual('@/context/TranslateContext');
+  return {
+    ...actual,
+    useTranslate: () => ({
+      language: DEFAULT_LANGUAGE,
+      availableLanguages: AVAILABLE_LANGUAGES,
+      t: (key: string, value?: string) => value ?? key,
+      setLanguage: vi.fn(),
+    }),
+  };
+});

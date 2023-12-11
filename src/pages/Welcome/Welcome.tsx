336d8e064182b1a950954b3982951152d05f4768
ElenaAnisimova <anilena232@mail.ru>
2023-12-11 12:35:07 +0800

    feat: add links to the buttons

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -4,10 +4,12 @@ import { Typography, Flex, Button } from 'antd';
 import TeamSection from '@/components/welcomePage/TeamSection/TeamSection';
 import FeaturesSection from '@/components/welcomePage/FeaturesSection/FeaturesSection';
 import CourseSection from '@/components/welcomePage/CourseSection/CourseSection';
-import { subHeadingStyles } from './WelcomePageConstants';
+import { subHeadingStyles, HEADINGS_TEXT, BUTTONS_PROPS } from './WelcomePageConstants';
+import { useNavigate } from 'react-router-dom';
 
 export function Welcome() {
   const { Title, Text } = Typography;
+  const navigate = useNavigate();
 
   return (
     <div className={styles.welcome__section}>
@@ -15,38 +17,54 @@ export function Welcome() {
         <Title className={styles.welcome__heading + ' ' + styles.main__heading}>
           Welcome to Setun-70<br></br>
         </Title>
+
         <Title
           className={styles.welcome__heading + ' ' + styles.main__heading}
           style={{ color: '#0046b8', marginTop: '0', marginBottom: '3rem' }}
         >
           GraphQL Playround!
         </Title>
+
         <Text style={subHeadingStyles}>
-          GraphQL Playground is a graphical, interactive, in-browser GraphQL IDE.<br></br> This tool
-          allows developers and clients to explore and interact with GraphQL APIs.
+          {HEADINGS_TEXT.first}
+          <br></br>
+          {HEADINGS_TEXT.second}
         </Text>
+
         <Flex gap="large" className={styles.button__wrapper}>
           <Button
+            onClick={() => {
+              navigate(BUTTONS_PROPS[0].link);
+            }}
             className={styles.welcome__btn + ' ' + styles.login__btn}
             type="primary"
-            size="large"
           >
-            Have an account? Log In
+            {BUTTONS_PROPS[0].text}
           </Button>
 
           <Button
+            onClick={() => {
+              navigate(BUTTONS_PROPS[1].link);
+            }}
             className={styles.welcome__btn + ' ' + styles.signup__btn}
             type="default"
-            size="large"
           >
-            Register now
+            {BUTTONS_PROPS[1].text}
           </Button>
         </Flex>
+
         <Flex justify="center">
-          <Button className={styles.welcome__btn + ' ' + styles.link__btn} type="link">
-            Explore GraphQL Playground
+          <Button
+            onClick={() => {
+              navigate(BUTTONS_PROPS[2].link);
+            }}
+            className={styles.welcome__btn + ' ' + styles.link__btn}
+            type="link"
+          >
+            {BUTTONS_PROPS[2].text}
           </Button>
         </Flex>
+
         <div className={styles.img__wrapper}>
           {/* TO DO: REPLACE WITH OUR PROJECT SCREENSHOT  */}
           <img
--- src/pages/Welcome/WelcomePageConstants.ts
+++ src/pages/Welcome/WelcomePageConstants.ts
@@ -2,3 +2,23 @@ export const subHeadingStyles = {
   fontSize: '26px',
   color: 'white',
 };
+
+export const HEADINGS_TEXT = {
+  first: 'GraphQL Playground is a graphical, interactive, in-browser GraphQL IDE.',
+  second: ' This tool allows developers and clients to explore and interact with GraphQL APIs.',
+};
+
+export const BUTTONS_PROPS = [
+  {
+    text: 'Have an account? Log In',
+    link: 'signin',
+  },
+  {
+    text: 'Register now',
+    link: 'signup',
+  },
+  {
+    text: 'Explore GraphQL Playground',
+    link: 'main',
+  },
+];

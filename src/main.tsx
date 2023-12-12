d8df8dc45c8cdc6dd8465a19b4fdf826a4588033
antasth <antasth@mail.ru>
2023-12-12 13:57:21 +0300

    feat: add firebase config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { App } from './app';
-
+import './firebase';
 import './index.scss';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

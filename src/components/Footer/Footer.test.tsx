714c2615a9ef543ffb0867a4b1a2847396b6490f
Iurii Bazhenov <bazhenovyn@gmail.com>
2023-12-13 18:16:37 +0100

    feat: delete links from the footer

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -3,15 +3,9 @@ import { MemoryRouter } from 'react-router-dom';
 import { Footer } from './Footer';
 
 describe('Footer', () => {
-  test('contain all pages of the application', () => {
+  test('renders correctly', () => {
     const { getByRole } = render(<Footer />, { wrapper: MemoryRouter });
-    const welcomeLink = getByRole('link', { name: /welcome/i });
-    const signInLink = getByRole('link', { name: /sign in/i });
-    const signUpLink = getByRole('link', { name: /sign up/i });
-    const mainLink = getByRole('link', { name: /main/i });
-    expect(welcomeLink).toBeInTheDocument();
-    expect(signInLink).toBeInTheDocument();
-    expect(signUpLink).toBeInTheDocument();
-    expect(mainLink).toBeInTheDocument();
+    const footer = getByRole('contentinfo');
+    expect(footer).toBeInTheDocument();
   });
 });
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,5 @@
-import {
-  CodeOutlined,
-  GithubOutlined,
-  HomeOutlined,
-  LoginOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-import { Button, Flex, Layout } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
+import { Flex, Layout } from 'antd';
 import { Link } from 'react-router-dom';
 import RssLogo from '@/assets/svg/rss.svg?react';
 import { useTranslate } from '@/context/TranslateContext';
@@ -15,20 +9,6 @@ export function Footer() {
   return (
     <Layout.Footer>
       <Flex vertical gap="small">
-        <Flex justify="center" wrap="wrap">
-          <Button type="link" icon={<HomeOutlined />} href="/">
-            {t('Application.Welcome', 'Welcome')}
-          </Button>
-          <Button type="link" icon={<CodeOutlined />} href="/">
-            {t('Application.Main', 'Main')}
-          </Button>
-          <Button type="link" icon={<LoginOutlined />} href="/">
-            {t('Application.SignIn', 'Sign in')}
-          </Button>
-          <Button type="link" icon={<UserOutlined />} href="/">
-            {t('Application.SignUp', 'Sign up')}
-          </Button>
-        </Flex>
         <Flex justify="center" align="center" gap="middle" wrap="wrap">
           <Link to="https://github.com/BazhenovYN/graphiql-app">
             <GithubOutlined style={{ fontSize: '30px' }} />

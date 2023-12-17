0ce0caa1fbb29ea803cdd88c9495aa95e17e7228
antasth <antasth@mail.ru>
2023-12-17 20:40:21 +0300

    feat: implement protected routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,21 @@
-import { RootLayout } from '@/layouts/RootLayout/RootLayout';
+import { RootLayout } from '@/layouts/RootLayout';
 import { SignIn } from '@/pages/SignIn';
 import { SignUp } from '@/pages/SignUp';
 import { Welcome } from '@/pages/Welcome';
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { AnonymousRoutes } from './AnonymousRoutes';
+import { PrivateRoutes } from './PrivateRoutes';
 
 export const routes = (
   <Route path="/" element={<RootLayout />}>
     <Route index element={<Welcome />} />
-    <Route path="/signin" element={<SignIn />} />
-    <Route path="/signup" element={<SignUp />} />
-    <Route path="/main" element={<p>Main Page</p>} />
+    <Route element={<PrivateRoutes />}>
+      <Route path="/main" element={<p>Main</p>} />
+    </Route>
+    <Route element={<AnonymousRoutes />}>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Route>
     <Route path="*" element={<p>404</p>} />
   </Route>
 );

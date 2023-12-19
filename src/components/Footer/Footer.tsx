5a7ed55e357edfb7d0915f896f586f1a8b7de6e9
ElenaAnisimova <anilena232@mail.ru>
2023-12-20 01:33:52 +0800

    chore: add team github links to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,11 @@
-import { GithubOutlined } from '@ant-design/icons';
 import { Flex, Layout } from 'antd';
 import { Link } from 'react-router-dom';
 import RssLogo from '@/assets/svg/rss.svg?react';
+import antasthGit from '@/assets/img/antasthGit.png';
+import bazhenovYNGit from '@/assets/img/bazhenovYNGit.png';
+import ElenaAnisimovaGit from '@/assets/img/ElenaAnisimovaGit.png';
 import { useTranslate } from '@/context/TranslateContext';
+import styles from './Footer.module.scss';
 
 export function Footer() {
   const { t } = useTranslate();
@@ -10,8 +13,14 @@ export function Footer() {
     <Layout.Footer>
       <Flex vertical gap="small">
         <Flex justify="center" align="center" gap="middle" wrap="wrap">
-          <Link to="https://github.com/BazhenovYN/graphiql-app">
-            <GithubOutlined style={{ fontSize: '30px' }} />
+          <Link to="https://github.com/BazhenovYN">
+            <img className={styles.img} src={bazhenovYNGit} alt="bazhenovYN-Github" />
+          </Link>
+          <Link to="https://github.com/antasth">
+            <img className={styles.img} src={antasthGit} alt="antasth-Github" />
+          </Link>
+          <Link to="https://github.com/ElenaAnisimova">
+            <img className={styles.img} src={ElenaAnisimovaGit} alt="ElenaAnisimova-Github" />
           </Link>
           <div>{t('Footer.About', '© 2023 Setun-70')}</div>
           <Link to="https://rs.school/js/">

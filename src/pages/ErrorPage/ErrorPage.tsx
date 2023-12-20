f2edfda1f5bc0b98020a81712b022be145c61c29
ElenaAnisimova <anilena232@mail.ru>
2023-12-21 01:51:27 +0800

    refactor: update link element on the error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,23 +1,17 @@
-import { useNavigate } from 'react-router-dom';
 import { Typography, Button } from 'antd';
 import styles from './ErrorPage.module.scss';
+import { Link } from 'react-router-dom';
 
 export function ErrorPage() {
   const { Title } = Typography;
-  const navigate = useNavigate();
-
   return (
     <div className={styles.errorWrapper} data-testid="error-page">
       <Title>AN ERROR HAS OCCURED</Title>
-      <Button
-        type="link"
-        className={styles.link}
-        onClick={() => {
-          navigate('/main');
-        }}
-      >
-        Go to the Main Page
-      </Button>
+      <Link to={'/main'}>
+        <Button type="link" className={styles.link}>
+          Go to the Main Page
+        </Button>
+      </Link>
     </div>
   );
 }

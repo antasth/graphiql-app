4b1e40255b6c992f6c168ecc1a338d3813fde309
Iurii Bazhenov <bazhenovyn@gmail.com>
2023-12-25 23:04:44 +0100

    fix: fix selected menu keys

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,22 @@
+import { useState } from 'react';
+
+import { CodeOutlined, HomeOutlined, LoginOutlined, UserOutlined } from '@ant-design/icons';
+import { Affix, Menu, type MenuProps } from 'antd';
+import { Header as AntdHeader } from 'antd/es/layout/layout';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import { useTranslate } from '@/context/TranslateContext';
 import { auth } from '@/firebase';
 import { useAuth } from '@/hooks/useAuth';
 import { useFirebase } from '@/hooks/useFirebase';
-import { CodeOutlined, HomeOutlined, LoginOutlined, UserOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Affix, Menu } from 'antd';
-import { Header as AntdHeader } from 'antd/es/layout/layout';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './Header.module.scss';
 import { LanguageSwitcher } from './LanguageSwitcher';
+
+import styles from './Header.module.scss';
 
 export const Header = () => {
   const [isAffixed, setIsAffixed] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
   const { t } = useTranslate();
   const { isAuth } = useAuth();
   const { signOutFromUserAccount } = useFirebase(auth);
@@ -30,12 +33,12 @@ export const Header = () => {
     menuItems.push(
       {
         label: t('Application.Main', 'Main'),
-        key: 'main',
+        key: '/main',
         icon: <CodeOutlined />,
       },
       {
         label: t('Application.SignOut', 'Sign out'),
-        key: 'signout',
+        key: '/signout',
         icon: <LoginOutlined />,
       }
     );
@@ -43,12 +46,12 @@ export const Header = () => {
     menuItems.push(
       {
         label: t('Application.SignIn', 'Sign in'),
-        key: 'signin',
+        key: '/signin',
         icon: <LoginOutlined />,
       },
       {
         label: t('Application.SignUp', 'Sign up'),
-        key: 'signup',
+        key: '/signup',
         icon: <UserOutlined />,
       }
     );
@@ -59,7 +62,7 @@ export const Header = () => {
   };
 
   const onClick: MenuProps['onClick'] = (e) => {
-    e.key === 'signout' ? handleLogOut() : navigate(e.key);
+    e.key === '/signout' ? handleLogOut() : navigate(e.key);
   };
 
   const handleAffixChange = (affixed?: boolean) => {
@@ -74,7 +77,13 @@ export const Header = () => {
         <div></div>
       </Affix>
       <AntdHeader className={`${styles.header} ${isAffixed ? styles.affixed : ''}`}>
-        <Menu onClick={onClick} mode="horizontal" items={menuItems} className={styles.menu} />
+        <Menu
+          onClick={onClick}
+          mode="horizontal"
+          items={menuItems}
+          selectedKeys={[location.pathname]}
+          className={styles.menu}
+        />
         <LanguageSwitcher />
       </AntdHeader>
     </>

bac22b55a451d57fe9f2e002719d46d5849d290a
ElenaAnisimova <anilena232@mail.ru>
2023-12-29 01:21:31 +0800

    refactor: delete console.error

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,14 +1,12 @@
 import { Typography, Button, Flex } from 'antd';
 import styles from './ErrorBoundary.module.scss';
 import { Link } from 'react-router-dom';
-import { useRouteError } from 'react-router-dom';
 import { useTranslate } from '@/context/TranslateContext';
 
 export function ErrorBoundary() {
-  const error = useRouteError();
   const { Title } = Typography;
   const { t } = useTranslate();
-  console.error(error);
+
   return (
     <Flex>
       <div className={styles.errorWrapper} data-testid="error-page">

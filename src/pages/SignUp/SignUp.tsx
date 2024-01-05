2c1c538c70a6b1719059754c4bcc392a066226da
antasth <antasth@mail.ru>
2024-01-05 13:32:25 +0300

    fix: change validation errors language in SignUp form on change app language

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,10 +1,11 @@
-import { useSignUpValidationSchema } from '@/constants/validation';
 import { useTranslate } from '@/context/TranslateContext';
 import { auth } from '@/firebase';
 import { useFirebase } from '@/hooks/useFirebase';
+import { useSignUpValidationSchema } from '@/hooks/useSignUpValidationSchema';
 import { ISignInValues } from '@/types';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Button, Form, Input } from 'antd';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { FormItem } from 'react-hook-form-antd';
 import { Link } from 'react-router-dom';
@@ -13,15 +14,28 @@ import styles from './SignUp.module.scss';
 export function SignUp() {
   const { t } = useTranslate();
   const { createUserWithEmailAndPassword } = useFirebase(auth);
+  const { validationSchema } = useSignUpValidationSchema();
 
-  const { control, handleSubmit } = useForm({
-    resolver: yupResolver(useSignUpValidationSchema()),
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    formState: { touchedFields },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
     mode: 'onChange',
   });
 
   const onFormSubmit = async ({ email, password }: ISignInValues) => {
     await createUserWithEmailAndPassword(email, password);
   };
+
+  useEffect(() => {
+    const isAnyFieldTouched = Object.values(touchedFields).some((field) => field);
+    if (isAnyFieldTouched) {
+      trigger();
+    }
+  }, [validationSchema, touchedFields, trigger]);
 
   return (
     <section className={styles.signUp}>

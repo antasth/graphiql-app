1ec7a1e2a5a1fe66604f1077289cd107b17b570b
antasth <antasth@mail.ru>
2024-01-05 21:20:26 +0300

    refactor: add index.ts for UserAuthListener component

--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -1,9 +1,9 @@
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
+import { UserAuthListener } from '@/components/UserAuthListener';
 import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
 import styles from './RootLayout.module.scss';
-import { UserAuthListener } from '@/components/UserAuthListener/UserAuthListener';
 
 export function RootLayout() {
   return (

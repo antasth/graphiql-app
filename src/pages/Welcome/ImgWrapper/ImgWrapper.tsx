952d2edb1a8b8ea602ac7dbb1f37d01828284a3b
ElenaAnisimova <anilena232@mail.ru>
2024-01-08 02:28:11 +0800

    chore: add link to youtube video to the welcome page

--- src/pages/Welcome/ImgWrapper/ImgWrapper.tsx
+++ src/pages/Welcome/ImgWrapper/ImgWrapper.tsx
@@ -1,7 +1,11 @@
 import PlaygroundScreenshot from '@/assets/img/screenshot.png';
 import styles from './ImgWrapper.module.scss';
+import { Flex } from 'antd';
+import { Link } from 'react-router-dom';
+import { useTranslate } from '@/context/TranslateContext';
 
 export function ImgWrapper() {
+  const { t } = useTranslate();
   return (
     <div className={styles.imgWrapper}>
       <img
@@ -9,6 +13,11 @@ export function ImgWrapper() {
         src={PlaygroundScreenshot}
         alt="playground-screenshot"
       />
+      <Flex justify="center" id={styles.linkBtnWrapper}>
+        <Link to={'https://www.youtube.com/watch?v=UnYo3C_sJmc'} id={styles.youtubeLink}>
+          {t('Welcome.Top.YoutubeLink')}
+        </Link>
+      </Flex>
     </div>
   );
 }
